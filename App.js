2db312bd5d65c15d9a3158dea5f89464ac56f8a7
anoopmundathan <anoop.mundathan@gmail.com>
2016-09-07 15:10:10 +0100

    Stateless component

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
-class App extends React.Component {
-	render() {
-		return <div>Hello from React Component</div>
-	}
-}
+const App = () => <div>Hello from React stateless component</div>
 
 export default App
